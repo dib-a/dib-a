90f77102eb8469b4eed1e919c9877953b36c6471
dib-a <96333029+dib-a@users.noreply.github.com>
2024-01-26 18:39:03 +0100

    update linking and favicon

--- popup.js
+++ popup.js
@@ -5,7 +5,7 @@ document.addEventListener('DOMContentLoaded', function() {
 
 function showPopup() {
     // Hier kannst du den Popup-Inhalt definieren
-    var popupContent = "Willkommen! Um die Webseite als App auf dem Smartphone zu sehen, teste sie im responsiven Design-Modus deines Browsers.";
+    var popupContent = "Willkommen! Dies ist eine Dummy Web-Applikation und dient nur der Veranschaulichung, daher sind viele Funktionen nicht implementiert <br> <br> Um die Webseite als App auf dem Smartphone zu sehen, teste sie im responsiven Design-Modus deines Browsers. <br><br> Unter Firefox: F12 dann das Smartphone-Symbol unten rechts";
 
     // Hier wird das Popup erstellt
     var popup = document.createElement('div');
@@ -15,8 +15,24 @@ function showPopup() {
     // Füge das Popup zum Body der Webseite hinzu
     document.body.appendChild(popup);
 
-    // Schließe das Popup nach einer gewissen Zeit (z.B., 10 Sekunden)
+    // Schließe das Popup nach einer gewissen Zeit (z.B., 20 Sekunden)
     setTimeout(function() {
+        closePopup();
+    }, 20000); // 20000 Millisekunden = 20 Sekunden
+
+    // Füge einen Event Listener für das Click-Ereignis hinzu
+    popup.addEventListener('click', function() {
+        closePopup();
+    });
+}
+
+function closePopup() {
+    // Hole das Popup-Element
+    var popup = document.querySelector('.popup');
+
+    // Überprüfe, ob das Popup-Element existiert, bevor du es entfernst
+    if (popup) {
+        // Entferne das Popup vom Body der Webseite
         document.body.removeChild(popup);
-    }, 10000); // 10000 Millisekunden = 10 Sekunden
+    }
 }
